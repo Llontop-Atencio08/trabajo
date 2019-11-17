6fddfea711adf44555d42da7df6d96603b5ad1e1
Llontop-Atencio08 <57873068+llontop-atencio08@users.noreply.github.com>
2019-11-17 18:35:00 -0500

    se agrego ejercicio de calculadora

---
+++ llontopcalculadora.py
@@ -0,0 +1,473 @@
+CALCULAR PROMEDIO DE IMPORTE DE PRODUCTOS DE LIBRERIA BAZAR
+#DECLARACION
+producto1,producto2,producto3=0.0,0.0,0.0
+promedio=0.0
+
+#INPUT
+producto1=250
+producto2=250
+producto3=250
+
+#PROCESSING
+promedio=(producto1+producto2+producto3)
+
+#OUTPUT
+print("#################")
+print("# IMPORTE TOTAL #")
+print("#################")
+print("# importe_total")
+
+
+#CALCULAR PROMEDIO DE VENTAS NETAS ENTRE LOS AÑO 2005 - 2010
+#DECLARACION
+anio2005,anio2006,anio2007,anio2008,anio2009,anio2010=0.0,0.0,0.0,0.0,0.0,0.0
+promedio=0.0
+
+#INPUT
+anio2005=15.321
+anio2006=18.249
+anio2007=20.893
+anio2008=20.131
+anio2009=14.544
+anio2010=15.287
+
+#PROCESING
+promedio=(anio2005+anio2006+anio2007+anio2008+anio2008+anio2009+anio2010)/6
+
+#OUTPUT
+print("####################")
+print("#   VENTAS NETAS   #")
+print("####################")
+print("#Promedio: ",promedio)
+print("####################")
+
+
+#CALCULAR PROMEDIO DE COSTO DE VENTAS ENTRE LOS AÑOS 2005 - 2010
+#DECLARACION
+anio2005,anio2006,anio2007,anio2008,anio2009,anio2010
+promedio=0.0
+
+#INPUT
+anio2005=9.271
+anio2006=11.649
+anio2007=13.868
+anio2008=13.1735
+anio2009=10.270
+anio2010=10.09
+
+#PROCESING
+promedio=(anio2005+anio2006+anio2007+anio2008+anio2008+anio2009+anio2010)/6
+
+#OUTPUT
+print("####################")
+print("# COSTOS DE VENTAS #")
+print("####################")
+print("#Promedio: ",promedio)
+print("####################")
+
+
+#PROMEDIO DE UTILIDAD BRUTA ENTRE LOS AÑOS 2005 -2010
+#DECLARACION
+anio2005,anio2006,anio2007,anio2008,anio2009,anio2010
+promedio=0.0
+
+#INPUT
+anio2005=6.050
+anio2006=6.600
+anio2007=7.025
+anio2008=6.396
+anio2009=4.274
+anio2010=3.95
+
+#PROCESSING
+promedio=(anio2005+anio2006+anio2007+anio2008+anio2008+anio2009+anio2010)/6
+
+#OUTPUT
+print("###################")
+print("# UTILIDAD BRUTA  #")
+print("###################")
+print("promedio= ",promedio)
+print("###################")
+
+
+#PROMEDIO DE GASTOS DE OPERACION ENTRE LOS AÑOS 2005 - 2010
+#DECLARACION
+anio2005,anio2006,anio2007,anio2008,anio2009,anio2010
+promedio=0.0
+
+#INPUT
+anio2005=3.563
+anio2006=3.655
+anio2007=4.130
+anio2008=4.069
+anio2009=3.109
+anio2010=3.08
+
+#PROCESSING
+promedio=(anio2005+anio2006+anio2007+anio2008+anio2008+anio2009+anio2010)/6
+
+#OUTPUT
+print("#######################")
+print("# GASTOS DE OPERACION #")
+print("#######################")
+print(  "Promedio= ",promedio  )
+print("#######################")
+
+
+#PROMEDIO DE RESULTADOS DE OPERACION ANTES DE OTROS GASTOS, NETO ENTRE LOS AÑOS 2005 - 2010
+#DECLARACION
+anio2005,anio2006,anio2007,anio2008,anio2009,anio2010
+promedio=0.0
+
+#INPUT
+anio2005=2.487
+anio2006=2.945
+anio2007=2.895
+anio2008=2.327
+anio2009=1.165
+anio2010=8.5
+
+#PROCESSING
+promedio=(anio2005+anio2006+anio2007+anio2008+anio2008+anio2009+anio2010)/6
+
+#OUTPUT
+print("###########################")
+print("# RESULTADOS DE OPERACION #")
+print("###########################")
+print(   "Promedio= ",promedio     )
+print("###########################")
+
+
+#PROMEDIO DE OTROS GASTOS, NETO ENTRE LOS AÑOS 2005 - 2010
+#DECLARACION
+anio2005,anio2006,anio2007,anio2008,anio2009,anio2010
+promedio=0.0
+
+#INPUT
+anio2005=316
+anio2006=49
+anio2007=273
+anio2008=1.909
+anio2009=407
+anio2010=50
+
+#PROCESSING
+promedio=(anio2005+anio2006+anio2007+anio2008+anio2008+anio2009+anio2010)/6
+
+#OUTPUT
+print("###################")
+print("#   OTROS GASTOS  #")
+print("###################")
+print("promedio= ",promedio)
+print("###################")
+
+
+#PROMEDIO DE GASTOS FINANCIEROS ENTRE LOS AÑOS 2005 - 2010
+#DECLARACION
+anio2005,anio2006,anio2007,anio2008,anio2009,anio2010
+promedio=0.0
+
+#INPUT
+anio2005=526
+anio2006=494
+anio2007=807
+anio2008=910
+anio2009=994
+anio2010=1.16
+
+#PROCESSING
+promedio=(anio2005+anio2006+anio2007+anio2008+anio2008+anio2009+anio2010)/6
+
+#OUTPUT
+print("######################")
+print("# GASTOS FINANCIEROS #")
+print("######################")
+print(  "Promedio: ",promedio )
+print("######################")
+
+
+#PROMEDIO DE OTROS GASTOS PRODUCTOS FINANCIEROS, NETO
+#DECLARACION
+anio2005,anio2006,anio2007,anio2008,anio2009,anio2010
+promedio=0.0
+
+#INPUT
+anio2005=765
+anio2006=462
+anio2007=900
+anio2008=1.617
+anio2009=117
+anio2010=4
+
+#PROCESSING
+promedio=(anio2005+anio2006+anio2007+anio2008+anio2008+anio2009+anio2010)/6
+
+#OUTPUT
+print("#############################")
+print("# OTROS PRODUCTOS FINANCIEROS")
+print("#############################")
+print(    "Promedio= ",promedio      )
+print("#############################")
+
+
+#PROMEDIO DE UTILIDAD PERDIDA ANTES DE IMPUESTOSA LA UTILIDAD ENTRE LOS AÑOS 2005 - 2010
+#DECLARACION
+anio2005,anio2006,anio2007,anio2008,anio2009,anio2010
+promedio=0.0
+
+#INPUT
+anio2005=2.495
+anio2006=2.989
+anio2007=2.851
+anio2008=2.031
+anio2009=341
+anio2010=89
+
+#PROCESSING
+promedio=(anio2005+anio2006+anio2007+anio2008+anio2008+anio2009+anio2010)/6
+
+#OUTPUT
+print("####################")
+print("# UTILIDAD PERDIDA #")
+print("####################")
+print("Promedio= ",promedio)
+print("####################")
+
+
+#PROMEDIO DE OPERACIONES DISCONTINUAS ENTRE LOS AÑOS 2005 - 2010
+#DECLARACION
+anio2005,anio2006,anio2007,anio2008,anio2009,anio2010
+promedio=0.0
+
+#INPUT
+anio2005=0
+anio2006=0
+anio2007=26
+anio2008=187
+anio2009=314
+anio2010=0
+
+#PROCESSING
+promedio=(anio2005+anio2006+anio2007+anio2008+anio2008+anio2009+anio2010)/6
+
+#OUTPUT
+print("############################")
+print("# OPERACIONES DISCONTINUAS #")
+print("############################")
+print("promedio= ",promedio)
+print("############################")
+
+
+#PROMEDIO DE PARTICIPACION NO CONTROLADORA
+#DECLARACION
+anio2005,anio2006,anio2007,anio2008,anio2009,anio2010
+promedio=0.0
+
+#INPUT
+anio2005=55
+anio2006=110
+anio2007=77
+anio2008=4
+anio2009=18
+anio2010=0
+
+#PROCESSING
+promedio=(anio2005+anio2006+anio2007+anio2008+anio2008+anio2009+anio2010)/6
+
+#OUTPUT
+print("##############################3##")
+print("# PARTICIPACION NO CONTROLADORA #")
+print("#################################")
+print("promedio= ",promedio)
+print("#################################")
+
+#PROMEDIO DE UTILIDAD PERDIDA NETA DE LA PARTICIPACION CONTROLADORA ENTRE LOS ANIOS 2005- 2010
+#DECLARACION
+anio2005,anio2006,anio2007,anio2008,anio2009,anio2010
+promedio=0.0
+
+#INPUT
+anio2005=2.112
+anio2006=2.378
+anio2007=2.391
+anio2008=203
+anio2009=104
+anio2010=1.06
+
+#PROCESSING
+promedio=(anio2005+anio2006+anio2007+anio2008+anio2008+anio2009+anio2010)/6
+
+#OUTPUT
+print("#########################")
+print("# UTILIDAD PERDIDA NETA #")
+print("#########################")
+print("promedio= ",promedio)
+print("#########################")
+
+
+#PROMEDIO DE MILLONES POR CPO EN CIRCULACION PROMEDIO ENTRE LOS ANIOS 2005 - 2010
+#DECLARACION
+anio2005,anio2006,anio2007,anio2008,anio2009,anio2010
+promedio=0.0
+
+#INPUT
+anio2005=6.919
+anio2006=7.184
+anio2007=7.432
+anio2008=8.381
+anio2009=8.932
+anio2010=11
+
+#PROCESSING
+promedio=(anio2005+anio2006+anio2007+anio2008+anio2008+anio2009+anio2010)/6
+
+#OUTPUT
+print("####################")
+print("# MILLONES POR CPO #")
+print("####################")
+print("promedio= ",promedio)
+print("####################")
+
+
+#PROMEDIO DE UTILIDAD PERDIDA POR CPO
+#DECLARACION
+anio2005,anio2006,anio2007,anio2008,anio2009,anio2010
+promedio=0.0
+
+#INPUT
+anio2005=0.31
+anio2006=0.33
+anio2007=0.32
+anio2008=0.02
+anio2009=0.01
+anio2010=0.2
+
+#PROCESSING
+promedio=(anio2005+anio2006+anio2007+anio2008+anio2008+anio2009+anio2010)/6
+
+#OUTPUT
+print("##########################")
+print("# UTILIDAD PERDIDA POR CPO")
+print("##########################")
+print("promedio= ",promedio)
+print("##########################")
+
+
+#PROMEDIO DE DIVIDENDOS POR CPO
+#DECLARACION
+anio2005,anio2006,anio2007,anio2008,anio2009,anio2010
+promedio=0.0
+
+#INPUT
+anio2005=0.06
+anio2006=0.09
+anio2007=0.08
+anio2008=0
+anio2009=0
+anio2010=0
+
+#PROCESSING
+promedio=(anio2005+anio2006+anio2007+anio2008+anio2008+anio2009+anio2010)/6
+
+#OUTPUT
+print("######################")
+print("# DIVIDENDOS POR CPO #")
+print("######################")
+print("promedio= ",promedio)
+print("######################")
+
+
+#PROMEDIO DE EFECTIVO Y EQUIVALENTES DE EFECTIVO
+#DECLARACION
+anio2005,anio2006,anio2007,anio2008,anio2009,anio2010
+promedio=0.0
+
+#INPUT
+anio2005=601
+anio2006=1.579
+anio2007=743
+anio2008=939
+anio2009=1.077
+anio2010=6
+
+#PROCESSING
+promedio=(anio2005+anio2006+anio2007+anio2008+anio2008+anio2009+anio2010)/6
+
+#OUTPUT
+print("########################################")
+print("# EFECTIVOS Y EQUIVALENTES DE EFECTIVO #")
+print("########################################")
+print("promedio= ",promedio)
+print("########################################")
+
+
+#PROMEDIO DE CAPITAL DE TRABAJO, NETO
+#DECLARACION
+anio2005,anio2006,anio2007,anio2008,anio2009,anio2010
+promedio=0.0
+
+#INPUT
+anio2005=1.268
+anio2006=887
+anio2007=1.383
+anio2008=1.191
+anio2009=946
+anio2010=1.5
+
+#PROCESSING
+promedio=(anio2005+anio2006+anio2007+anio2008+anio2008+anio2009+anio2010)/6
+
+#OUTPUT
+print("###########################")
+print("# CAPITAL DE TRABAJO NETO #")
+print("###########################")
+print("promedio= ",promedio)
+print("###########################")
+
+
+#PROMEDIO DE INMUEBLES, MAQUINARIA Y EQUIPO, NETO
+#DECLARACION
+anio2005,anio2006,anio2007,anio2008,anio2009,anio2010
+promedio=0.0
+
+#INPUT
+anio2005=15.542
+anio2006=17.196
+anio2007=22.895
+anio2008=19.671
+anio2009=19.776
+anio2010=17.9
+
+#PROCESSING
+promedio=(anio2005+anio2006+anio2007+anio2008+anio2008+anio2009+anio2010)/6
+
+#OUTPUT
+print("####################################")
+print("# INMUEBLES MAQUINARIA Y EQUIPO NETO")
+print("####################################")
+print("promedio= ",promedio)
+print("####################################")
+
+
+#PROMEDIO TOTAL ACTIVOS ENTRE LOS AÑOS 2005 - 2010
+#DECLARACION
+anio2005,anio2006,anio2007,anio2008,anio2009,anio2010
+promedio=0.0
+
+#INPUT
+anio2005=26.763
+anio2006=29.972
+anio2007=49.662
+anio2008=45.387
+anio2009=44.483
+anio2010=40.8
+
+#PROCESSING
+promedio=(anio2005+anio2006+anio2007+anio2008+anio2008+anio2009+anio2010)/6
+
+#OUTPUT
+print("###################")
+print("#  TOTAL ACTIVOS  #")
+print("###################")
+print("promedio= ",promedio)
+print("###################")
